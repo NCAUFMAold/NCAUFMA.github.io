7dea6ae818edbf83bc41a48300c0eb5290873223
Vitor Gabriel <vitor.grs2004@gmail.com>
2024-11-01 20:29:00 -0300

    atualizaçõa de implementação a acess  secretkey

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -3,17 +3,9 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase
 import { getAuth, setPersistence, browserLocalPersistence, GoogleAuthProvider, signInWithPopup, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js";
 import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
 
+import firebaseConfig from './firebase-config.js'; // Ajuste o caminho conforme necessário
 
 
-// Configuração do Firebase
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-};
 
 
 // Inicializa o Firebase e configura a autenticação
